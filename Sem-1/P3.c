a886c878ab865d2d01f5dedf3e35959b92cfc7c5
Shreyansh <shreyansh1104@gmail.com>
2024-10-31 01:38:39 +0530

    Third Program Done

---
+++ Sem-1/P3.c
@@ -0,0 +1,15 @@
+/*Write a C program to swap two variables using a third variable.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 3, y = 2, tmp;
+    printf("x = %d, and y = %d\n", x, y);
+    tmp = x;
+    x = y;
+    y = tmp;
+    printf("x = %d, and y = %d\n", x, y);
+    printf("By Shreyansh Singh");
+    return 0;
+}
