9e6da972d78ae7398d8337ce95eacaa9bc2adb5f
Shreyansh <shreyansh1104@gmail.com>
2024-10-31 01:55:53 +0530

    Fifth Program Done

---
+++ Sem-1/P5.c
@@ -0,0 +1,16 @@
+/*Write a C program to convert a given integer (in seconds) in hours, minutes and seconds.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 3600, s, m, h, i = 60;
+    s = x % i;
+    x /= i;
+    m = x % i;
+    x /= i;
+    h = x % i;
+    printf("%d hours, %d minutes and %d seconds left\n", h, m, s);
+    printf("By Shreyansh Singh\n");
+    return 0;
+}
