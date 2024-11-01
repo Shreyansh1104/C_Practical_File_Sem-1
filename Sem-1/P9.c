ee17357d25194099b2faa747449f8d7346d03842
Shreyansh <shreyansh1104@gmail.com>
2024-11-01 20:55:38 +0530

    Ninth Program Done

---
+++ Sem-1/P9.c
@@ -0,0 +1,23 @@
+/*Write a C program to check whether a triangle is equilateral, scalene or isoceles.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 3, y = 4, z = 5;
+
+    if (x == y && y == z)
+    {
+        printf("Equilateral Triangle\n");
+    }
+    else if (x == y || y == z || x == z)
+    {
+        printf("Isoceles Triangle\n");
+    }
+    else
+    {
+        printf("Scalene Triangle\n");
+    }
+    printf("By Shreyansh Singh\n");
+    return 0;
+}
