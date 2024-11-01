778b03e8f682a7b6ac24704dc4a12f20e50b824b
Shreyansh <shreyansh1104@gmail.com>
2024-11-01 20:28:05 +0530

    Sixth Program Done

---
+++ Sem-1/P6.c
@@ -0,0 +1,17 @@
+/*Write a C program to convert specified days into years, weeks and days.*/
+
+#include<stdio.h>
+
+int main()
+{
+    int x = 363, d, w, y, i = 365, j = 7;
+
+    y = x / i;
+    x = x % i;
+    w = x / j;
+    x = x % j;
+    d = x;
+    printf("%d years, %d weeks and %d days later\n", y, w, d);
+    printf("By Shreyansh Singh\n");
+    return 0;
+}
