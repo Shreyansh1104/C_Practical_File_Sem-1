166bb14f24e8898f1f934a60453a635954d90db3
Shreyansh <shreyansh1104@gmail.com>
2024-11-02 17:25:14 +0530

    Fifteenth Program Done

---
+++ Sem-1/P15.c
@@ -0,0 +1,16 @@
+/*Write a C program to find largest of three numbers.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 3, y = 6, z = 9;
+    if (x > y && x > z)
+    printf("%d is largest\n", x);
+    else if (y > x && y > z)
+    printf("%d is largest\n", y);
+    else
+    printf("%d is largest\n", z);
+    printf("By Shreyansh Singh\n");
+    return 0;
+}
