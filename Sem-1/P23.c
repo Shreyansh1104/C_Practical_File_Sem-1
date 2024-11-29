a133ede62d212929cb04344711bbc853591362c6
Shreyansh1104 <shreyansh1104@gmail.com>
2024-11-29 16:04:45 +0530

    Program 23 Actually Done

--- Sem-1/P23.c
+++ Sem-1/P23.c
@@ -1,1 +1,47 @@
 /*Write a program in C to print Fibonacci sequence 0 1 1 2 3 5 8 13 ... N terms and prints the sum of sequence.*/
+
+#include <stdio.h>
+
+int add(int a, int b);
+int main()
+{
+    int n, i, sum, x, y, z;
+    printf("Enter no. of terms:");
+    scanf("%d", &n);
+    switch(n)
+    {
+        case 0:
+        printf("0 terms in sequence, hence, sum cannot be obtained");
+        break;
+        default:
+        for (i = 0; i < n; i++)
+        {
+            if (i < 2)
+            {
+                x = i;
+                y = 0;
+                z = add(x, y);
+                printf("%d\n", z);
+                sum += z;
+            }
+            else
+            {
+                z = add(x, y);
+                y = x;
+                x = z;
+                printf("%d\n", z);
+                sum += z;
+            }
+        }
+        printf("Sum of Sequence is %d", sum);
+        break;
+    }
+    return 0;
+}
+
+int add(int a, int b)
+{
+    int sum;
+    sum = a + b;
+    return sum;
+}
