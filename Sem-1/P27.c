b3d3d8c69522df45d47c815c32cb0e988ff05442
Shreyansh <shreyansh1104@gmail.com>
2024-12-01 19:13:44 +0530

    Program 27 Done

--- Sem-1/P27.c
+++ Sem-1/P27.c
@@ -1,1 +1,34 @@
 /*Write a program in C to accept an interger number and to check a number is Perfect or not.*/
+
+#include <stdio.h>
+
+int perf(int x);
+int main()
+{
+    int x;
+    printf("Enter number:");
+    scanf("%d", &x);
+    if (x == perf(x))
+    {
+        printf("The number %d is a Perfect number", x);
+    }
+    else
+    {
+        printf("The number %d is not a Perfect number", x);
+    }
+    return 0;
+}
+
+int perf(int x)
+{
+    int y = 0;
+    for (int i = 1; i < x; i++)
+    {
+        if (x % i == 0)
+        {
+            y += i;
+            printf("%d\n", y);
+        }
+    }
+    return y;
+}
