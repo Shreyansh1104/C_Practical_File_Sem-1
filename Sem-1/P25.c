9dfeb468f05ec91cd75e5606814c400791c5ba06
Shreyansh <shreyansh1104@gmail.com>
2024-12-01 18:34:11 +0530

    Program 25 Done

--- Sem-1/P25.c
+++ Sem-1/P25.c
@@ -1,1 +1,33 @@
 /*Write a program in C to accept an integer number and find reverse of this number and check this number for palindrome.*/
+
+#include <stdio.h>
+
+int rev(int x);
+int main()
+{
+    int x;
+    printf("Enter number");
+    scanf("%d", &x);
+    if (x == rev(x))
+    {
+        printf("The number %d is a palindrome", x);
+    }
+    else
+    {
+        printf("The number %d is not a palindrome", x);
+    }
+    return 0;
+}
+
+int rev(int x)
+{
+    int y = 0, rem;
+    do
+    {
+        rem = x % 10;
+        y = y * 10 + rem;
+        x /= 10;
+    }
+    while (x != 0);
+    return y;
+}
