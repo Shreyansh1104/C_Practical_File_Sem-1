c7683a15df6e01903fd37dc3851841fefabe33ee
Shreyansh1104 <shreyansh1104@gmail.com>
2024-12-06 15:48:17 +0530

    Program 32 Done

--- Sem-1/P32.c
+++ Sem-1/P32.c
@@ -1,6 +1,19 @@
 /*Write a program to convert binary number to decimal number.*/
 
+#include <stdio.h>
+#include <math.h>
+
 int main()
 {
+  int b_int, d_int = 0, exp = 0;
+  printf("Enter binary no.");
+  scanf("%d", &b_int);
+  while (b_int != 0)
+  {
+    d_int += (b_int % 10) * pow(2, exp);
+    b_int /= 10;
+    exp += 1;
+  }
+  printf("Decimal form: %d", d_int);
   return 0;
 }
