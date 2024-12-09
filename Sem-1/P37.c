4430cdc8ac0cd5d33df8f2a3193194785b32a55d
Shreyansh1104 <shreyansh1104@gmail.com>
2024-12-09 09:00:41 +0530

    Program 37 Done

---
+++ Sem-1/P37.c
@@ -0,0 +1,28 @@
+/*Write a program in C to print the following pattern:
+    1
+    0   1
+    1   0   1
+    0   1   0   1
+    1   0   1   0   1
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, x = 3;
+    for (i = 1; i <= x; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (i % 2 == 0)
+            {
+                printf("%d\t", (j + 1) % 2);
+            }
+            else
+            printf("%d\t", j % 2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
